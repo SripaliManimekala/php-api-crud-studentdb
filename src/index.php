75319d5cdc9be33dea48ae5ca3490b8f9eb26124
SripaliManimekala <sripalimanimekala@gmail.com>
2023-08-21 11:54:13 +0530

    connected to the studentdb database-index.php

--- src/index.php
+++ src/index.php
@@ -1,9 +1,16 @@
 <?php
+$host = "mysql_db";
+$username = "root";
+$password = "root";
+$db_name = "studentdb";
 
-
-$con = new mysqli('mysql_db','root','root','studentdb');
-// if($con->connect_error){
-
-if ($con){
-    echo "connected to the database";
+// Create connection
+$conn = mysqli_connect($host,$username, $password,$db_name);
+//$con = new mysqli('mysql_db','root','root','studentdb');
+if(!$conn){
+    die("Connection failed: ".mysqli_connect_error());
 }
+else{
+	echo "Connected Succesfully to the database";
+}
+?>
