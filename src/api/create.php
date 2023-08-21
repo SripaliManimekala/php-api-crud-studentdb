71ba4e121b2e9a6e20cdc4f5a6575fe3678c15cf
SripaliManimekala <sripalimanimekala@gmail.com>
2023-08-21 20:03:51 +0530

    geting started with create.php API

--- src/api/create.php
+++ src/api/create.php
@@ -1,6 +1,28 @@
 <?php
-require_once 'index.php'; //include the database connection
+//  
+include("../index.php"); //include the database connection
 header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json");
+header('Access-Control-Allow-Method: POST');
+
+
+$requestMethod =$_SERVER["REQUEST_METHOD"];
+
+if($requestMethod == "POST"){
+    
+    $input_data= json_decode(file_get_contents("php://input"), true); //for any data
+    echo $input_data['name'];  
+    
+    
+}
+else{
+    //print error message
+    $data = [
+        'status' => 405,
+        'message'=> $requestMethod .' Invalid request method',
+    ];
+    header("HTTP/1.0 405 Method Not Allowed");
+    echo json_encode($data);
+}
 
 ?>
